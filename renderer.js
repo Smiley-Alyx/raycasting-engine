a55d66aebc6a3629360b2bfaa0ef7d1d48af0517
Alexandra Shornikova <smiley-alyx@yandex.ru>
2026-03-17 13:33:11 +0100

    refactor(renderer): extract drawing into renderer module

---
+++ renderer.js
@@ -0,0 +1,55 @@
+import { getTextureForMaterial } from './materials.js';
+import { getMap } from './map-state.js';
+
+export function createRenderer({ ctx, getViewWidth, getViewHeight, player }) {
+  function drawBackground() {
+    const w = getViewWidth();
+    const h = getViewHeight();
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = '#E3E3E1';
+    ctx.fillRect(0, 0, w, h / 2);
+    ctx.fillStyle = '#858585';
+    ctx.fillRect(0, h / 2, w, h / 2);
+  }
+
+  function drawRay(dist, x, offset, img) {
+    const viewWidth = getViewWidth();
+    const viewHeight = getViewHeight();
+    const distanceProjectionPlane = (viewWidth / 2) / Math.tan(player.fov / 2);
+    const sliceHeight = (1 / dist) * distanceProjectionPlane;
+
+    const texture = getTextureForMaterial(img);
+    if (!texture) return;
+
+    let texX = Math.floor(offset * 512);
+    if (texX < 0) texX = 0;
+    if (texX > 511) texX = 511;
+
+    ctx.drawImage(texture, texX, 0, 1, 512, x, viewHeight / 2 - sliceHeight / 2, 1, sliceHeight);
+  }
+
+  function drawMap() {
+    const map = getMap();
+    if (!map) return;
+
+    ctx.clearRect(0, 0, map[0].length * 5, map.length * 5);
+    ctx.fillStyle = 'rgb(255, 0, 0)';
+    ctx.fillRect(player.x * 5 - 1, player.y * 5 - 1, 2, 2);
+
+    for (let y = 0; y < map.length; y++) {
+      for (let x = 0; x < map[y].length; x++) {
+        if (map[y][x] > 0) {
+          ctx.fillStyle = 'rgb(0, 0, 0)';
+          ctx.fillRect(x * 5, y * 5, 5, 5);
+        }
+      }
+    }
+  }
+
+  return {
+    drawBackground,
+    drawRay,
+    drawMap,
+  };
+}
